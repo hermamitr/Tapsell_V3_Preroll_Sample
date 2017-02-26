fd369e197f0e355c699f42ad8900008df0bdaf61
rezaei <sinarezaei1991@gmail.com>
2017-02-26 11:26:09 +0330

    First Commit

---
+++ app/src/main/java/ir/tapsell/samplev3preroll/MainActivity.java
@@ -0,0 +1,56 @@
+package ir.tapsell.samplev3preroll;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import org.nexage.sourcekit.vast.VASTPlayer;
+
+import ir.tapsell.sdk.Tapsell;
+import ir.tapsell.sdk.vast.TapsellVast;
+
+public class MainActivity extends AppCompatActivity implements VASTPlayer.VASTPlayerListener{
+
+    VASTPlayer vastPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Tapsell.initialize(MainActivity.this,"ctdfiktagpkrldtcmltepobendhrbhtcpoahnecajlmcbattranjoengrnptksjastpeea");
+        vastPlayer = new VASTPlayer(MainActivity.this, MainActivity.this);
+
+        findViewById(R.id.btnShow).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                vastPlayer.loadVideoWithUrl(TapsellVast.getVastUrl(MainActivity.this,"58b284904684651bf8196cbc",TapsellVast.VAST_VERSION_3));
+            }
+        });
+    }
+
+    @Override
+    public void vastReady() {
+        vastPlayer.play();
+    }
+
+    @Override
+    public void vastError(int error) {
+
+    }
+
+    @Override
+    public void vastClick() {
+
+    }
+
+    @Override
+    public void vastComplete() {
+
+    }
+
+    @Override
+    public void vastDismiss() {
+
+    }
+}
